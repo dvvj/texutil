7bda7a7ecd3a741b8aaed13428b26037a17d3f37
new2scala <jiajiwu.icare@gmail.com>
2018-12-14 15:43:03 +0100

    t

--- exutil1/src/test/scala/org/ditw/exutil1/naen/NaEnMatcherTests.scala
+++ exutil1/src/test/scala/org/ditw/exutil1/naen/NaEnMatcherTests.scala
@@ -9,6 +9,10 @@ class NaEnMatcherTests extends FlatSpec with Matchers with TableDrivenPropertyCh
   private val testData = Table(
     ("inStr", "expNEIds"),
     (
+      "aurora behavioral healthcare",
+      Set(2000000268L, 2000000269L, 2000000270L)
+    ),
+    (
       "UNIVERSITY OF DELAWARE",
       Set(1000006652L)
     )
