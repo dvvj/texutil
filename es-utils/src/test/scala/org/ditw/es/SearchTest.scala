1d8a55c9874154c52451d975a26ae33c53c8f6a2
new2scala <jiajiwu.icare@gmail.com>
2019-02-05 10:17:02 +0100

    es search util

--- es-utils/src/test/scala/org/ditw/es/SearchTest.scala
+++ es-utils/src/test/scala/org/ditw/es/SearchTest.scala
@@ -1,6 +1,7 @@
 package org.ditw.es
 import java.net.InetAddress
 
+import org.ditw.es.utils.EsUtils
 import org.elasticsearch.action.search.SearchType
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
@@ -10,23 +11,9 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient
 
 object SearchTest extends App {
 
-  val settings = Settings.builder().put(
-    "cluster.name", "my-app"
-  ).build()
+  val clt = EsUtils.localClient("my-app")
 
-  val clt = new PreBuiltTransportClient(settings)
-  clt.addTransportAddress(
-    new TransportAddress(
-      InetAddress.getByName("localhost"), 9300
-    )
-  )
-
-  val resp = clt.prepareSearch("td_idx")
-    .setSearchType(SearchType.DEFAULT)
-    .setQuery(QueryBuilders.matchAllQuery())
-    .setSize(15)
-    .execute()
-    .actionGet()
+  val resp = EsUtils.runSearch(clt, "td_idx", QueryBuilders.matchAllQuery(), 15)
 
   val hits = resp.getHits.getHits
   println(s"${hits.size}")
